94a96e2a841e5ffcaa95789519d091fb0ae80f2d
AbiR7171 <abiremtiag@gmail.com>
2024-02-14 16:10:18 +0600

    project setup completed

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express"
+const app : Application = express()
+
+
+app.get('/', (req : Request, res : Response) => {
+
+  const a = 10;
+  res.send(a)
+})
+
+
+export default app;
+
